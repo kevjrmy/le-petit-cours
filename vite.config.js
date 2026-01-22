e194f08f1efccd0b1ae543664344a6ce2c2ba8d6
kevjrmy <kevin.jgnetworks@gmail.com>
2026-01-22 20:30:24 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -51,6 +51,7 @@ export default defineConfig({
       },
 
       workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
         globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
         cleanupOutdatedCaches: true,
         clientsClaim: true,
